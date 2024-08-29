ead7dd3566c7d2f54013e563bf3742decf3c0e94
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-29 16:21:16 +0600

    card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Navbar from "../../components/navbar";
 import Banner from "../../components/Banner";
 import Footer from "../../components/Footer";
+import Card from "../../components/Card";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      {/* card container */}
+      <div className="container grid col-span-12 mx-auto my-10">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Footer />
     </div>
   );
