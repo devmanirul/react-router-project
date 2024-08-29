b7e9b995bb325dba517e4a7372c104b4d5a5e93a
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-29 19:40:08 +0600

    card daynamic

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Card = (image, description, title) => {
+const Card = ({ image, description, title }) => {
+  console.log({ image, description, title });
   return (
-    <div className="card grid col-span-4 w-full bg-base-100 shadow-xl">
-      <figure>
-        <img src={image} alt="Shoes" />
+    <div className="card grid col-span-4 gap-2 w-full bg-base-100 shadow-xl">
+      <figure className="w-full">
+        <img src={image} alt="Shoes" className="h-96 w-full" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
