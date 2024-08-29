a988c9f9bf070c8887c4e36f58cb3290e7358f95
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-08-29 19:28:33 +0600

    card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
-const Card = () => {
+const Card = (image, description, title) => {
   return (
     <div className="card grid col-span-4 w-full bg-base-100 shadow-xl">
       <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
+        <img src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{title}</h2>
+        <p>{description}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar";
 import Banner from "../../components/Banner";
 import Footer from "../../components/Footer";
 import Card from "../../components/Card";
 
+// fetch("https://fakestoreapi.com/products/1")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Banner />
       {/* card container */}
       <div className="container grid grid-cols-12 gap-10 mx-auto my-10">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {products.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            image={item.image}
+          />
+        ))}
       </div>
       <Footer />
     </div>
